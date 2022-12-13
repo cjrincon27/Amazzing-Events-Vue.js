2c136123672861b152fe2d9c6bbff3dd48c7d9b6
Carlos Rincon <boloncahs@gmail.com>
2022-12-13 13:00:57 -0500

    Delete CARLOS .RINCON_JS_TASK_4 directory

--- CARLOS .RINCON_JS_TASK_4/js/details.js
+++
@@ -1,8 +0,0 @@
-const info=data.events;//guardo el array de eventos para details 
-const params= new URLSearchParams(location.search) ;//parametros de la pagina details
-const id=params.get('id');//id enviado a la pagina details
-const contenedor=document.getElementById('containerdetails');//conyenedor details
-
-cardgeneratord(foundelement(id,info))//recibe como parametro la card
-
-//---------funtions--------------
